163928405bf3a20461ebf41f600589697da3b940
AhmedElawady <a.elawady303@gmail.com>
2019-09-21 06:13:21 +0200

    working on UIView

--- Smack/Controller/AvatarPickerViewControler.swift
+++ Smack/Controller/AvatarPickerViewControler.swift
@@ -12,8 +12,10 @@ class AvatarPickerViewControler: UIViewController, UICollectionViewDelegate, UIC
     
     // Outlets
     @IBOutlet weak var collectionView: UICollectionView!
-    
     @IBOutlet weak var segmentControl: UISegmentedControl!
+    
+    // Variables
+    var avatarType = AvatarType.dark
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +26,7 @@ class AvatarPickerViewControler: UIViewController, UICollectionViewDelegate, UIC
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "avatarCell", for: indexPath) as? AvatarCell {
+            cell.configureCell(index: indexPath.item, type: avatarType)
             return cell
         }
         return AvatarCell()
@@ -38,7 +41,37 @@ class AvatarPickerViewControler: UIViewController, UICollectionViewDelegate, UIC
     }
 
     @IBAction func segmentControlChanges(_ sender: Any) {
+        if segmentControl.selectedSegmentIndex == 0 {
+            avatarType = .dark
+        } else {
+            avatarType = .light
+        }
+        collectionView.reloadData()
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        var numberOfColumns: CGFloat = 3
+        if UIScreen.main.bounds.width > 320 {
+            numberOfColumns = 4
+        }
+        
+        let spaceBetweenCells: CGFloat = 10
+        let padding: CGFloat = 40
+        let cellDimension = ((collectionView.bounds.width - padding) - (numberOfColumns - 1) * spaceBetweenCells) / numberOfColumns
+        
+        return CGSize(width: cellDimension, height: cellDimension)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if avatarType == .dark {
+            UserDataService.instance.setAvatarName(avatarName: "dark\(indexPath.item)")
+        } else {
+            UserDataService.instance.setAvatarName(avatarName: "light\(indexPath.item)")
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     @IBAction func backPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
--- Smack/Controller/LogUpViewController.swift
+++ Smack/Controller/LogUpViewController.swift
@@ -14,17 +14,32 @@ class LogUpViewController: UIViewController {
     @IBOutlet weak var emailText: UITextField!
     @IBOutlet weak var passwordText: UITextField!
     @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
     
     // Variables
     var avatarName = "profileDefault"
     var avatarColor = "[0.5, 0.5, 0.5, 1]"
+    var backGroundColor: UIColor?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setUpView()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDataService.instance.avatarName != "" {
+            userImage.image = UIImage(named: UserDataService.instance.avatarName)
+            avatarName = UserDataService.instance.avatarName
+            if avatarName.contains("light") && backGroundColor == nil {
+                userImage.backgroundColor = UIColor.lightGray
+            }
+        }
     }
     
     @IBAction func logUpPressed(_ sender: UIButton) {
+        spinner.isHidden = false
+        spinner.startAnimating()
+        
         guard let name = userNameText.text, userNameText.text != "" else
         { return }
         guard let email = emailText.text, emailText.text != "" else
@@ -38,8 +53,10 @@ class LogUpViewController: UIViewController {
                     if success {
                         AuthServices.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
                             if success {
-                                print(UserDataService.instance.name, UserDataService.instance.avatarName)
+                                self.spinner.isHidden = true
+                                self.spinner.stopAnimating()
                                 self.performSegue(withIdentifier: UNWIND, sender: nil)
+                                NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
                             }
                         })
                     }
@@ -53,10 +70,32 @@ class LogUpViewController: UIViewController {
     }
     
     @IBAction func chooseBackColorPressed(_ sender: Any) {
+        let red = CGFloat(arc4random_uniform(255)) / 255
+        let green = CGFloat(arc4random_uniform(255)) / 255
+        let blue = CGFloat(arc4random_uniform(255)) / 255
+        
+      backGroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        UIView.animate(withDuration: 0.2) {
+            self.userImage.backgroundColor = self.backGroundColor
+        }
     }
     
     @IBAction func closePressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
     }
     
+    func setUpView() {
+        spinner.isHidden = true
+        
+        userNameText.attributedPlaceholder = NSAttributedString(string: "username", attributes: [NSAttributedString.Key.foregroundColor:smackPurplePlaceHolder])
+        emailText.attributedPlaceholder = NSAttributedString(string: "email", attributes: [NSAttributedString.Key.foregroundColor:smackPurplePlaceHolder])
+        passwordText.attributedPlaceholder = NSAttributedString(string: "password", attributes: [NSAttributedString.Key.foregroundColor:smackPurplePlaceHolder])
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(LogUpViewController.handleTap))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func handleTap() {
+        view.endEditing(true)
+    }
 }
